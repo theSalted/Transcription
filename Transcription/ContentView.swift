02b8fc5748836fd3ebfab3a2d9b7e22ea3944bfd
Yuhao Chen <yuhao.chen2000@icloud.com>
2024-05-29 00:27:18 +0800

    Initial Commit

---
+++ Transcription/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Transcription
+//
+//  Created by Yuhao Chen on 5/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Transcription/TranscriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriptionApp.swift
+//  Transcription
+//
+//  Created by Yuhao Chen on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TranscriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
